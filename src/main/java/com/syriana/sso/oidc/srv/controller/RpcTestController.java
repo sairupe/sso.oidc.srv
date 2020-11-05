67a72905d38385e7c136785bb98c1e9a54ecfee8
zhongheng <260789672@qq.com>
2020-11-05 17:42:45 +0800

    【修改】RPC请求时间打印

--- src/main/java/com/syriana/sso/oidc/srv/controller/RpcTestController.java
+++ src/main/java/com/syriana/sso/oidc/srv/controller/RpcTestController.java
@@ -2,6 +2,8 @@ package com.syriana.sso.oidc.srv.controller;
 
 import com.syriana.sso.oidc.srv.request.rpctest.RpcTestReqDto;
 import com.syriana.sso.oidc.srv.response.rpctest.RpcTestResDto;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author syriana.zh
  * @date 2020/06/22
  */
+@Slf4j
 @RestController
 @RequestMapping("/rpc")
 public class RpcTestController {
 
     @PostMapping("/test")
     public RpcTestResDto rpc(@RequestBody RpcTestReqDto queryDto) {
+        log.info(new DateTime().toString());
         RpcTestResDto result = new RpcTestResDto();
         if (queryDto != null) {
             result.setUserName(queryDto.getUserName());
